a170ee321710c940aac855ebaf85dac25b5058d1
Bhagavanr <bhagavan.kalavapalli@gmail.com>
2022-05-17 09:52:20 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_kafka_producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is .NET core kafka producer app");
+        }
+    }
+}
